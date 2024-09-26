110b9062b8dc024b1b70c0c26c698977b920bc5f
solunska <a_solunska@outlook.com>
2024-09-26 19:20:29 +0200

    resolved 404 error for missing script.js file and deployed updates

--- src/UI/ShoeSize.jsx
+++ src/UI/ShoeSize.jsx
@@ -6,7 +6,7 @@ export default function ShoeSize({ size, selectedSize, setSelectedSize, disabled
     return <label
         className={`${isSelected ? classes.selectedSize : classes.size} ${disabled ? classes.disabled : ''}`}
         onClick={() => setSelectedSize(size)}>
-        <input type='radio' name='size' className={classes.radio} checked={isSelected} disabled={disabled} />
+        <input type='radio' name='size' className={classes.radio} checked={isSelected} disabled={disabled} onChange={() => {}} />
         {size}
     </label>
 }
